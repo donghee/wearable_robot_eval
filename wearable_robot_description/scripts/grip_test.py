0498f834dc122b31a9653685d766e0abd4c40fe4
Donghee Park <dongheepark@gmail.com>
2022-10-31 18:14:12 +0900

    Add grip test

---
+++ wearable_robot_description/scripts/grip_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64MultiArray
+
+
+class Publisher(Node):
+
+    def __init__(self):
+        super().__init__('effort_test_node')
+        self.pub = self.create_publisher(
+                Float64MultiArray, 
+                '/effort_controllers/commands', 
+                10)
+        timer_period = 1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 200.0
+
+    def timer_callback(self):
+        msg = Float64MultiArray()
+        msg.data=[self.i]
+        self.pub.publish(msg)
+        self.get_logger().info('Published: "%s"' % msg.data)
+        self.i *= -1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    my_node = Publisher()
+
+    rclpy.spin(my_node)
+
+    my_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
