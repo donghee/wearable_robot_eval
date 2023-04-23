d200a7610335928a83edb09fd62d660b9e8aca7b
Donghee Park <dongheepark@gmail.com>
2023-04-24 04:53:25 +0900

    Add force parameter in werable robot controller

--- wearable_robot_gazebo/src/wearable_robot_controller_plugin.cpp
+++ wearable_robot_gazebo/src/wearable_robot_controller_plugin.cpp
@@ -4,12 +4,29 @@
 #include <gazebo/common/common.hh>
 #include <ignition/math/Vector3.hh>
 
+#include <gazebo_ros/node.hpp>
+
+
 namespace gazebo
 {
   class WearableRobot : public ModelPlugin
   {
-    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+
+    gazebo_ros::Node::SharedPtr ros_node_;
+    ignition::math::Vector3d force_;
+
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
     {
+      this->ros_node_ = gazebo_ros::Node::Get(_sdf);
+      this->force_ = ignition::math::Vector3d(0, 0, 0);
+
+      if (_sdf->HasElement("force")) {
+        this->force_ = _sdf->Get<ignition::math::Vector3d>("force");
+        RCLCPP_INFO(
+            this->ros_node_->get_logger(),
+            "Set Force: %f %f %f", this->force_.X(), this->force_.Y(), this->force_.Z());
+      }
+
       this->model = _parent;
 
       this->updateConnection = event::Events::ConnectWorldUpdateBegin(
@@ -18,7 +35,13 @@ namespace gazebo
 
     public: void OnUpdate()
     {
-      this->model->GetLink("link")->SetForce(ignition::math::Vector3d(1.0, 0.0, 0.0));
+      this->model->GetLink("link")->SetForce(this->force_);
+
+      ignition::math::Vector3d relative_force = this->model->GetLink("link")->RelativeForce();
+
+      RCLCPP_INFO(
+          this->ros_node_->get_logger(),
+          "Read Force: %f %f %f", relative_force.X(), relative_force.Y(), relative_force.Z());
     }
 
     private: physics::ModelPtr model;
