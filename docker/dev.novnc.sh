b377625c009e56446d4721040bb572d8169c2c9b
Donghee Park <dongheepark@gmail.com>
2023-06-21 05:23:23 +0900

    WIP: Fix black screen

--- docker/dev.novnc.sh
+++ docker/dev.novnc.sh
@@ -2,7 +2,8 @@
 
 docker run -it \
     --gpus all \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -e DISPLAY=:1 \
+    --volume="/tmp/.X11-unix/X0:/tmp/.X11-unix/X0:rw" \
     --volume="$HOME/ros2_ws:/root/dev_ws:rw" \
     --volume="$HOME/.ssh:/root/.ssh:ro" \
     --rm \
@@ -10,6 +11,8 @@ docker run -it \
     -p 5901:5901 \
     --workdir="/root" \
     ghcr.io/donghee/wearable_robot_eval:foxy_nvidia_novnc \
-    bash -c "TVNC_VGL=1 /opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :1 && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:5901"
+    bash -c "touch ~/.Xauthority;xauth generate :0 . trusted;/opt/TurboVNC/bin/vncserver -wm xfce -SecurityTypes None :1 && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:5901"
+    #bash -c "touch ~/.Xauthority;xauth generate :0 . trusted;/opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :1 && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:5901"
+    #bash -c "TVNC_VGL=1 /opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :1 && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:5901"
     #bash -c "TVNC_VGL=1 /opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :1 && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ --token-plugin=TokenFile --token-source=/root/.vnc/websockify-token.cfg 6080"
     #bash -c "/root/ros2_ws/src/wearable_robot_eval/docker/novnc.sh"
