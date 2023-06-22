29d4a1bb0e8ef682fdefd07596c70dc00d146ded
Donghee Park <dongheepark@gmail.com>
2023-06-23 02:17:04 +0900

    WIP: update dev.novnc.sh

--- docker/dev.novnc.sh
+++ docker/dev.novnc.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
+ID=9
+NOVNC_PORT=$((8080 + $ID))
+VNC_PORT=$((5900 + $ID))
+
 docker run -it \
     --gpus all \
-    -e DISPLAY=:1 \
     --volume="/tmp/.X11-unix/X0:/tmp/.X11-unix/X0:rw" \
     --volume="$HOME/ros2_ws:/root/dev_ws:rw" \
     --volume="$HOME/.ssh:/root/.ssh:ro" \
     --rm \
     -p 80:6080 \
-    -p 5901:5901 \
+    -p $NOVNC_PORT:6080 \
+    -p $VNC_PORT:$VNC_PORT \
     --workdir="/root" \
     ghcr.io/donghee/wearable_robot_eval:foxy_nvidia_novnc \
-    bash -c "touch ~/.Xauthority;xauth generate :0 . trusted;/opt/TurboVNC/bin/vncserver -wm xfce -SecurityTypes None :1 && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:5901"
-    #bash -c "touch ~/.Xauthority;xauth generate :0 . trusted;/opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :1 && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:5901"
-    #bash -c "TVNC_VGL=1 /opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :1 && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:5901"
-    #bash -c "TVNC_VGL=1 /opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :1 && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ --token-plugin=TokenFile --token-source=/root/.vnc/websockify-token.cfg 6080"
-    #bash -c "/root/ros2_ws/src/wearable_robot_eval/docker/novnc.sh"
+    bash -c "/opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :$ID && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:$VNC_PORT"
+    #bash -c "/opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :1 && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ --token-plugin=TokenFile --token-source=/root/.vnc/websockify-token.cfg 6080"
