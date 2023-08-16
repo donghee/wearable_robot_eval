31cbdea4929a9861c8ac73a45e1b9dbd4700bef1
Donghee Park <dongheepark@gmail.com>
2023-08-17 00:09:42 +0900

    Update run.eval.*

--- run.eval.py
+++ run.eval.py
@@ -3,11 +3,10 @@ import signal
 import subprocess
 import time
 from time import sleep
-from tqdm import tqdm
 from wearable_robot_evaluation.wearability.simulation_modify_2 import test_safety
 from wearable_robot_evaluation.interactivity.interaction_score import test_interactivity
 
-def stop_process(proc, timeout=10):
+def kill_process(proc, timeout=10):
     pgid = os.getpgid(proc.pid)
     time.sleep(timeout)
     os.killpg(pgid, signal.SIGINT)
@@ -16,37 +15,42 @@ def stop_process(proc, timeout=10):
     os.system('pkill gzclient')
     proc.wait()
 
-
 # Run simulation
-evaluation_proc = subprocess.Popen(['ros2', 'launch', 'wearable_robot_gazebo', 'evaluation.launch.py'], start_new_session=True)
-stop_process(evaluation_proc, 80)
+#evaluation_proc = subprocess.Popen(['ros2', 'launch', 'wearable_robot_gazebo', 'evaluation.launch.py'], start_new_session=True)
+#kill_process(evaluation_proc, 80)
 
 # Wearability
 os.system('clear')
 print("Start Wearability")
 test_safety(0.0, 150.0)
 print("Finish Wearability")
-#time.sleep(2)
-for i in tqdm(range(5)):
-    sleep(1)
+
+print("-----------------------------")
+input("\nPress enter to continue...\n")
 
 # Interactivity
 os.system('clear')
 print("Start Interactivity")
 test_interactivity()
 print("Finish Interactivity")
-#time.sleep(5)
-for i in tqdm(range(5)):
-    sleep(1)
+
+print("-----------------------------")
+input("\nPress enter to continue...\n")
 
 # Usability
 os.system('clear')
 print("Start Usability")
 usability_proc = subprocess.Popen(['./act-r.sh', '10'], cwd='./wearable_robot_evaluation/usability', stdin=subprocess.PIPE)
-usability_proc.stdin.write(b'\x04') # Send ctrl+d to stop ACT-R
+time.sleep(2)
+usability_proc.stdin.write(b'(load "/home/donghee/src/wearable_robot_act-r/tmp/actr7.x/usability/system_interface.lisp")\n')
+time.sleep(1)
+#usability_proc.stdin.write(b'(run-the-model-until-time 10 (list \"arm\" \"adaptive\") 39 \"high\" nil 20 5)\n')
+usability_proc.stdin.write(b'(run-the-model-until-time 10 (list "arm" "adaptive") 39 "high" nil 20 5)\n')
+usability_proc.stdin.close() # Send EOF to stop ACT-R
 time.sleep(2)
 print("Finish Usability")
-for i in tqdm(range(5)):
-    sleep(1)
+
+print("-----------------------------")
+input("\nPress enter to continue...\n")
 
 print("Finish Evaluation")
--- run.eval.sh
+++ run.eval.sh
@@ -5,18 +5,6 @@ colcon build
 cd ~/ros2_ws/src/wearable_robot_eval
 . ~/ros2_ws/install/setup.bash
 
-# Run Simulation and Wearability, Interactivity
+# Run Simulation and Wearability, Interactivity, Usability Evaluation
 clear
 python3 run.eval.py
-
-# Usability
-#clear
-#echo "Start Usability"
-#pushd ./wearable_robot_evaluation/usability
-#./act-r.sh 10 &
-#PID_ACT_R=$!
-## Wait for 2 seconds
-#sleep 2
-#kill $PID_ACT_R
-#popd
-#echo "Finish Usability"
