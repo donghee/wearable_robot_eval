6a7de1d7b549ae69bbf75160d00abfb535650207
Donghee <dongheepark@gmail.com>
2023-08-24 18:13:06 +0900

    Hotfix docker update (#4)
    * Add python library and lisp sbcl
    
    * Delete
    
    * Fix Dockerfile

--- docker/dev.sh
+++ docker/dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+#
 usage()
 {
   echo "Usage: ./`basename $0` 1~8"
@@ -11,19 +11,22 @@ then
   usage 0
 fi
 
-
 ID=$@
-NOVNC_PORT=$((8080 + $ID))
+NOVNC_PORT=$((6080 + $ID))
 VNC_PORT=$((5900 + $ID))
 
-#--workdir="/root" \
 docker run -it \
     --gpus all \
-    --privileged \
+    --privileged  \
     --volume="/tmp/.X11-unix/X0:/tmp/.X11-unix/X0:rw" \
-		--volume="${HOME}/src:${HOME}/src" \
+    --volume="$HOME/ros2_ws:$HOME/ros2_ws:rw" \
+    --volume="$HOME/src:$HOME/src:rw" \
+    --volume="$HOME/.ssh:$HOME/.ssh:ro" \
+    --workdir="${HOME}" \
     --rm \
     -p $NOVNC_PORT:6080 \
     -p $VNC_PORT:$VNC_PORT \
     ghcr.io/donghee/wearable_robot_eval:foxy_nvidia_novnc_user \
     bash -c "/bootstrap.sh && sudo chmod 777 -R /tmp/.X11-unix && /opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :$ID && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:$VNC_PORT"
+    
+#    bash -c "sudo chmod 777 -R /tmp/.X11-unix && /opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :$ID && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:$VNC_PORT"
