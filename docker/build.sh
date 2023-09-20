e6e9d9bff77029feba27c4fd3812a5102ea826d6
Donghee Park <dongheepark@gmail.com>
2023-09-20 21:29:00 +0900

    Update docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 # root
-docker build -t ghcr.io/donghee/wearable_robot_eval:foxy_nvidia_novnc . -f Dockerfile
+#docker build -t ghcr.io/donghee/wearable_robot_eval:foxy_nvidia_novnc . -f Dockerfile
 
 # user
-docker build -t ghcr.io/donghee/wearable_robot_eval:foxy_nvidia_novnc_user --build-arg="USER=donghee" --build-arg="HOME=/home/donghee" . -f Dockerfile
+docker build -t ghcr.io/donghee/wearable_robot_eval:foxy_nvidia_novnc_user_$USER --build-arg="UID=$(id -u)" --build-arg="GID=$(id -g)" --build-arg="USER=$USER" --build-arg="HOME=$HOME" . -f Dockerfile
