3092732375942f3d7eb9fd32172f1281722b11fd
Donghee Park <dongheepark@gmail.com>
2023-09-20 21:29:00 +0900

    Update docker

--- docker/dev.sh
+++ docker/dev.sh
@@ -23,10 +23,12 @@ docker run -it \
     --volume="$HOME/src:$HOME/src:rw" \
     --volume="$HOME/.ssh:$HOME/.ssh:ro" \
     --workdir="${HOME}" \
+    --user="$(id -u):$(id -g)" \
     --rm \
+    -p 80:80 \
     -p $NOVNC_PORT:6080 \
     -p $VNC_PORT:$VNC_PORT \
-    ghcr.io/donghee/wearable_robot_eval:foxy_nvidia_novnc_user \
+    ghcr.io/donghee/wearable_robot_eval:foxy_nvidia_novnc_user_$USER \
     bash -c "/bootstrap.sh && sudo chmod 777 -R /tmp/.X11-unix && /opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :$ID && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:$VNC_PORT"
     
 #    bash -c "sudo chmod 777 -R /tmp/.X11-unix && /opt/TurboVNC/bin/vncserver -wm LXDE -SecurityTypes None :$ID && /opt/noVNC/utils/websockify/run --verbose --web=/opt/noVNC/ 6080 127.0.0.1:$VNC_PORT"
