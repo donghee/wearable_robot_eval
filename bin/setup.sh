63466ffef2a5a0ee30b717d562802d7ff0cd19f4
Donghee Park <dongheepark@gmail.com>
2025-11-14 07:45:42 +0900

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -13,10 +13,14 @@ else
     echo 'Docker is already installed.'
 fi
 
+# Create Index.dat to prepare frontend 
+touch Index.dat
+mkdir -p Patient
+
 sh ./wearable_ui_frontend/tools/setup.sh
 sh ./wearable_ui_backend/tools/setup.sh
 sh ./wearable_robot_mujoco/docker/setup.sh
-sh ./wearable_vscode_extention/docker/setup.sh
+sh ./wearable_vscode_extention/tools/setup.sh
 
 # Install latest VSCode
 wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O vscode.deb
