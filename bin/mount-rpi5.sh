e96d14e13388d71c3eb2d5b1c5df99f5778f6408
Donghee Park <dongheepark@gmail.com>
2026-07-06 10:27:17 +0900

    Update interactivity module

--- bin/mount-rpi5.sh
+++ bin/mount-rpi5.sh
@@ -2,6 +2,8 @@
 
 sudo umount ~/wearable_robot_eval/wearable_robot_upper_limb
 mkdir -p ~/wearable_robot_eval/wearable_robot_upper_limb
-sshfs donghee@100.90.168.18:dynamixel_ws/src/wearable_robot_upper_limb/ ~/wearable_robot_eval/wearable_robot_upper_limb
+#sshfs donghee@100.90.168.18:dynamixel_ws/src/wearable_robot_upper_limb/ ~/wearable_robot_eval/wearable_robot_upper_limb
+#sshfs donghee@192.168.88.26:dynamixel_ws/src/wearable_robot_upper_limb/ ~/wearable_robot_eval/wearable_robot_upper_limb
+sshfs donghee@172.30.1.41:dynamixel_ws/src/wearable_robot_upper_limb/ ~/wearable_robot_eval/wearable_robot_upper_limb
 
 df
